4cd570374596c317f69045c0d2360da7392932f3
邓涵杰 <87105391+floatDreamWithSong@users.noreply.github.com>
2024-11-22 17:33:31 +0800

    adjust: 更正内容描述

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
-    readonly VITE_REPORTE_URL: string;
+    readonly VITE_REPORT_URL: string;
     readonly VITE_USER_NAME: string;
 }
 
